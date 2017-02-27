4aac86283b2f6d4b1c1e403036aaf59a8896ef7a
pawell67 <pawellwankiewicz@gmail.com>
2017-02-27 22:35:17 +0100

    added todo and calculator to works section added owl carousel to works section

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('.item').on('blur',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+});
--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $to = "pawelwankiewicz@wp.pl";
 $name = $_POST['name'];
 $message = $_POST['message'];
-$headers = 'From: ' . $name . '(' . $_POST["email"] . ')' \r\n . 'Content-type: text/html; charset=utf-8';
+$headers = 'From: ' . $name . '(' . $_POST["email"] . ')' . '/r/n' . 'Content-type: text/html; charset=utf-8';
 
 mail($to, $name, $message, $headers);
 
