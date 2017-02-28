9aab5a739411a9930c19fa93bfa22e55c233faf5
pawell67 <pawellwankiewicz@gmail.com>
2017-02-28 21:59:09 +0100

    changed background, fixed some other issues.

--- js/type.js
+++ js/type.js
@@ -1,7 +1,7 @@
 $(window).on("load", function () {
     setTimeout(function () {
         $(".my-name").typed({
-            strings: ["Paweł Wankiewicz<br><div class='sss'></div> Front-end Developer"],
+            strings: ["Paweł Wankiewicz<br></div> Front-end Developer"],
             typeSpeed: 75
         });
     }, 500);
