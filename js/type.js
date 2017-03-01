a133b7a546e60624cc81cd37b4b77ade621ada09
pawell67 <pawellwankiewicz@gmail.com>
2017-03-01 17:01:43 +0100

    some responsibility issues fixed

--- js/type.js
+++ js/type.js
@@ -2,7 +2,7 @@ $(window).on("load", function () {
     setTimeout(function () {
         $(".my-name").typed({
             strings: ["Paweł Wankiewicz<br></div> Front-end Developer"],
-            typeSpeed: 75
+            typeSpeed: 50
         });
     }, 500);
 });
